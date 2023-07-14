a2b944f77838f024329b36e89e02f036281e0ce6
EfraimGhiurau <efy009@gmail.com>
2023-07-14 20:15:38 +0300

    iframe width fix

--- arhiva/arhiva.js
+++ arhiva/arhiva.js
@@ -12,11 +12,34 @@ const search_button = document.querySelector('.search-button').addEventListener(
 function getArchive() {
   var month = document.querySelector("#month").value ;
   var year = document.querySelector("#year").value;
+  var bodyWidth = window.innerWidth;
+  var columnsNumber;
+  if (bodyWidth > 1000) {
+    columnsNumber = 5;
+  } else if (bodyWidth > 800) {
+    columnsNumber = 4;
+  } else if (bodyWidth > 600) {
+    columnsNumber = 3;
+  } else {
+    columnsNumber = 2;
+  }
 
   var archiveContainer = document.querySelector("#archive-container");
   archiveContainer.innerHTML = 
   `<iframe scrolling="auto" frameborder="1"
-    src="http://archives.bisericilive.com/gallery?cid=sperantaoradearo&f=${year}/${month}"
+    src="http://archives.bisericilive.com/gallery?cid=sperantaoradearo`+
+    `&bgc=a2a2a2`+
+    `&c=${columnsNumber}`+
+    `&gc=000`+
+    `&esc=fff`+
+    `&f=${year}/${month}"
     allowtransparency="false"></iframe>`;
-    
 }
+
+
+/*
+// Old iframe:
+<iframe scrolling="auto" frameborder="0" 
+src="http://archives.bisericilive.com/gallery?cid=sperantaoradearo&amp;c=4&amp;bgc=fff&amp;gc=555&amp;esc=ddd&amp;esch=ccc" 
+allowtransparency="false"></iframe>
+*/
